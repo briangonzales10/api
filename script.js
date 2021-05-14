14c60b517f9203059df2eb68368109e70e89fec9
briangonzales10 <brian.gonzales10@gmail.com>
2021-05-14 17:02:26 -0500

    updates

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ form.addEventListener("submit", getGif)
 
 function getGif(evt) {
     evt.preventDefault();
-    
+
     const API = "https://api.giphy.com/v1/gifs/search?api_key=DuM5deN971gS5WhyqEDc8RJNX4OdB1bJ&q="
     const searchTerm = form.searchTerm.value;
     const searchURL = `${API}${searchTerm}`
@@ -17,9 +17,13 @@ function getGif(evt) {
 }
 
 function displayGifs(mygifs) {
+    
+    document.getElementById("gifcont").innerHTML = "";
+
     for (let gif of mygifs) {
         const gifTitle = gif.title;
         const gifURL = gif.images.downsized_small.mp4;
+    
         
         const box = document.createElement("div")
         box.setAttribute("class", "gifBox")
@@ -27,6 +31,7 @@ function displayGifs(mygifs) {
                         <video src="${gifURL}" alt="${gifTitle}" />
         `;
 
-        document.getElementById("gifcont").appendChild(box); 
+        document.getElementById("gifcont").appendChild(box);
+        
     }
 }
